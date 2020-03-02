c5104c9fa8e31cee4a012d724137727fc271bfab
Julia Brazolim <julia_hammett.j.d@hotmail.com>
2020-03-02 19:56:51 -0300

    Código para formulário

---
+++ Formulário/Formulario.py
@@ -0,0 +1,12 @@
+print('Preencha o formulario a seguir')
+
+nome = input("Insira seu nome completo: ")
+cpf = input("Insira o número do seu CPF: ")
+end = input("Insira seu endereço: ")
+idade = input("Insira sua idade: ")
+altura = input("Insira sua altura: ")
+telefone = input("Ïnsira seu número de telefone: ")
+
+print("\n Dados cadastrados com sucesso! \n Nome: \t", nome, "\n CPF: \t", cpf, "\n Endereço: \t", end, "\n Idade: \t", idade, "anos", "\n Altura: \t", altura, "\n Telefone: \t", telefone)
+
+
