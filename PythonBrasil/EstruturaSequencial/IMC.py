77b4803e75e6e5afe6823155be7af5e40552b792
Julia Brazolim <julia_hammett.j.d@hotmail.com>
2020-03-04 20:41:31 -0300

    Cálculo IMC

---
+++ PythonBrasil/EstruturaSequencial/IMC.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+print('Calculo de IMC')
+altura = float(input('\n Insira o valor da sua altura: '))
+IMC = (72.7 * altura) - 58
+
+print('Seu peso ideal é de ', round(IMC, 1), 'kilos')
+
