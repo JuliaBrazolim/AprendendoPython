4ceb9a1ae383c275cec960ab33433bfdefac3d63
Julia Brazolim <julia_hammett.j.d@hotmail.com>
2020-03-04 20:26:20 -0300

    Calculo Produto
    "o produto do dobro do primeiro com metade do segundo", me perdi mas acho que deu certo

---
+++ PythonBrasil/EstruturaSequencial/calcProduto.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+num1 = int(input('Insira um número: '))
+num2 = int(input('Insira outro número: '))
+num3 = float(input('Insira outro número: '))
+dobro = (num1 * 2)
+metade = (num2 / 2)
+a = dobro + metade
+print('\n O produto do dobro do primeiro com metade do segundo é', a)
+
+b = ((num1 * 3) + num3)
+print('\n Agora, a soma do triplo do primeiro com o terceiro é', b)
+
+c = num3 * num3 * num3
+print('\n O terceiro elevado ao cubo', c)
