6da4f2b2ed6a51727428c11a01e99e5d34e1b34f
Julia Brazolim <julia_hammett.j.d@hotmail.com>
2020-03-04 13:56:48 -0300

    Converter metros para cm

---
+++ PythonBrasil/EstruturaSequencial/converterMcm.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+print('Conversor de metros para centímetros')
+
+metros = float(input('Insira quantos metros você quer converter para centimetros: '))
+converter = (metros * 100)
+
+print(metros, 'metros em centimetros vale ', float(converter), 'centimetros')
