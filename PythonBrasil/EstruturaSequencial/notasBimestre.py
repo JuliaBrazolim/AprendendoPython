c8c328fb082aeccbf6d3054dd016b47c4d5e288b
Julia Brazolim <julia_hammett.j.d@hotmail.com>
2020-03-04 13:40:27 -0300

    add exer4

---
+++ PythonBrasil/EstruturaSequencial/notasBimestre.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+print('Vamos calcular sua média escolar!')
+
+bim1 = float(input('Insira a sua nota do primeiro bimestre: '))
+bim2 = float(input('Insira a sua nota do segundo bimestre: '))
+bim3 = float(input('Insira a sua nota do terceiro bimestre: '))
+bim4 = float(input('Insira a sua nota do quarto bimestre: '))
+media = ((bim1 + bim2 + bim3 + bim4) / 4)
+
+print('A sua média é: ', media)
