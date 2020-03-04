2784b62e6be0046694b212b96a036ba092bfc5b1
Julia Brazolim <julia_hammett.j.d@hotmail.com>
2020-03-04 13:20:52 -0300

    add exercicio 3

---
+++ PythonBrasil/EstruturaSequencial/doisNumeros.py
@@ -0,0 +1,6 @@
+# -*- coding: utf-8 -*-
+num1 = float(input('Insira um numero: '))
+num2 = float(input('Insira um outro numero: '))
+soma = (num1 + num2)
+
+print('O resultado da soma é: ', float(soma))
