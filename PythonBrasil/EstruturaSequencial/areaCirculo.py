6b223049084326804e5182f42170ec0d5d58606d
Julia Brazolim <julia_hammett.j.d@hotmail.com>
2020-03-04 15:21:01 -0300

    Calcular área de um círculo

---
+++ PythonBrasil/EstruturaSequencial/areaCirculo.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+print('Calcular a area de um círculo')
+
+raio = float(input('Insira o valor do raio: '))
+area = 3.14 * (raio * raio)
+
+print('A área do círculo é de: ', float(area))
