f99de35e48616ea6414aef6e6465f6e11b4d5bf9
Julia Brazolim <julia_hammett.j.d@hotmail.com>
2020-03-04 16:09:49 -0300

    Celsius para Fahrenheit

---
+++ PythonBrasil/EstruturaSequencial/CforF.py
@@ -0,0 +1,5 @@
+# -*- coding: utf-8 -*-
+print('Passando graus Celsius para Fahrenheit')
+C = float(input('Insira o valor da temperatura em graus Celsius: '))
+temp = (C - 32) * 5/9
+print('\n A temperatura em Fahrenheit é de:', round(temp, 1))
