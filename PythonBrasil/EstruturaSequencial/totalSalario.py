53a1871adf9f2cda6c3aa03cb920d8052fa719d3
Julia Brazolim <julia_hammett.j.d@hotmail.com>
2020-03-04 15:40:38 -0300

    Salário Mensal

---
+++ PythonBrasil/EstruturaSequencial/totalSalario.py
@@ -0,0 +1,6 @@
+# -*- coding: utf-8 -*-
+ganhoHora = float(input('Quanto você ganha por hora? '))
+horasTrab = float(input('E por quantas horas você trabalha em um mês? '))
+
+salario = ganhoHora * horasTrab
+print('Seu salário mensal é de ', salario)
