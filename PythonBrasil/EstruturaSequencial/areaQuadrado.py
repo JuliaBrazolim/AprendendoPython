5050bddfa1f7c4d0fef755d1cf452556bcc7a58c
Julia Brazolim <julia_hammett.j.d@hotmail.com>
2020-03-04 15:34:39 -0300

    Área do quadrado e seu dobro

---
+++ PythonBrasil/EstruturaSequencial/areaQuadrado.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+print('Calcular a área de um quadrado')
+
+lado = float(input('Insira o valor de um dos lados: '))
+area = (lado * lado)
+dobro = 2 * lado
+
+print('A área do quadrado é de ', area)
+print('\n mas, o dobro da área é de: ', dobro)
+
+
