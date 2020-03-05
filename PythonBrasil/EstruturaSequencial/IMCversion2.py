1f363a41f00f9caeaeff6a7745a1ef72ec1b6b02
Julia Brazolim <julia_hammett.j.d@hotmail.com>
2020-03-04 21:11:19 -0300

    IMC Segunda Versão

---
+++ PythonBrasil/EstruturaSequencial/IMCversion2.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+print('Calculo de IMC -- Segunda versão')
+h = float(input('\n Insira o valor de sua altura: '))
+homens = (72.7 * h) - 58
+mulheres = (62.1 * h) - 44.7
+
+print('\n O resultado para homens é de ', homens)
+print('\n O resultado para mulheres é de ', mulheres)
+
