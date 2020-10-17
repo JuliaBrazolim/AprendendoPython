8eff042a11294fbfe020ddfcd174d63b6b537698
Julia Brazolim <juliabrazolim@gmail.com>
2020-10-17 13:46:37 -0300

    Corrigido

--- PythonBrasil/EstruturaDeDecisao/maiorTres.py
+++ PythonBrasil/EstruturaDeDecisao/maiorTres.py
@@ -2,9 +2,9 @@ a = float(input('Insira um numero: '))
 b = float(input('Insira um numero: '))
 c = float(input('Insira um numero: '))
 
-if a > b and c:
-    print('o numero', a, 'é o maior')
-elif c > a and b:
-    print('o numero', c, 'é o maior')
+if a > b > c:
+    print('o numero', a, 'é o maior dos três valores')
+elif b > a < c:
+    print('o numero', b, 'é o maior dos três valores')
 else:
-    print('o maior numero é', b)
+    print('o maior numero dos três é', c)
