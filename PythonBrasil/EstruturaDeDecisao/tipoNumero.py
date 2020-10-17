3b55c2d52cafa1893e14fb5d8127a7eac3396a6b
Julia Brazolim <juliabrazolim@gmail.com>
2020-10-17 14:42:20 -0300

    Tipo de número
    Retorna se o número inserido é negativo, positivo ou zero

---
+++ PythonBrasil/EstruturaDeDecisao/tipoNumero.py
@@ -0,0 +1,7 @@
+a = int(input('Insira um numero: '))
+if a <= -1:
+    print('O numero', a, 'é negativo')
+elif a == 0:
+    print('O numero', a, 'é zero')
+else:
+    print('O numero', a, 'é positivo')
