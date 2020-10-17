1ef139f6d9160ee6a716197dfe1965e55a95709c
Julia Brazolim <juliabrazolim@gmail.com>
2020-10-17 14:39:56 -0300

    Vogal e consoante
    Insira uma letra e o programa retorna o tipo

---
+++ PythonBrasil/EstruturaDeDecisao/tipoLetra.py
@@ -0,0 +1,6 @@
+letra = str(input('Insira uma letra: '))
+
+if letra == 'a' or letra == 'e' or letra == 'i' or letra == 'o' or letra == 'u':
+    print('A letra', letra, 'é vogal')
+else:
+    print('A letra', letra, 'é consoante')
